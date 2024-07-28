c0a3e7c61c0223a7f4a33637b182d75d2589677b
Vanmarcos22 <marc.anthony.22699@gmail.com>
2024-07-28 18:35:17 +0200

    Making some changes

--- src/data_processing/load_data.py
+++ src/data_processing/load_data.py
@@ -1,5 +1,25 @@
 from sklearn.model_selection import train_test_split
 
+def create_train_test_split(X, y, test_size, random_state=42):
+    """
+    Splits the dataset into training and testing sets.
+
+    Parameters:
+    - X: Features.
+    - y: Labels.
+    - test_size: Size of the test set (can be an int or a float representing the proportion).
+    - random_state: Seed for reproducibility. Default is 42.
+
+    Returns:
+    - X_train: Training features.
+    - X_test: Testing features.
+    - y_train: Training labels.
+    - y_test: Testing labels.
+    """
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    return X_train, X_test, y_train, y_test
+
+
 def split_data_by_test_sizes(X_train, y_train, test_sizes, random_state=42):
     """
     Splits the training data into new training sets and pools based on provided test sizes.
